5a40fc72e1409fe75e153fd37edb7cf54881d689
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-10-02 11:24:24 -0300

    instalação do cors no backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const mongooseConnect = require('./mongooseConnect');
 const routes = require('./routes');
 
@@ -10,6 +12,7 @@ mongoose.connect(mongooseConnect, {
   useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
