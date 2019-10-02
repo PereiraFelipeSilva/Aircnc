5175e7f4ecea804ecab87d658ce3c8d1541f225b
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-10-02 12:39:21 -0300

    utilização do virtuals do mongoose pra fornecer a url das thumbnails

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const mongooseConnect = require('./mongooseConnect');
 const routes = require('./routes');
@@ -14,6 +15,7 @@ mongoose.connect(mongooseConnect, {
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
