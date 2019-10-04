ea7cd9448063aaeebf9b483b76c005973162bcd5
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-10-04 17:32:26 -0300

    finalização da comunicação com o mobile e projeto da semana concluído

--- mobile/src/pages/List/List.js
+++ mobile/src/pages/List/List.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import socketio from 'socket.io-client';
 import {
   SafeAreaView,
   ScrollView,
   AsyncStorage,
   Image,
-  TouchableOpacity
+  TouchableOpacity,
+  Alert
 } from 'react-native';
 
 import styles from './styles';
@@ -13,6 +15,18 @@ import SpotList from '../../components/spotList/SpotList';
 
 export default function List({ navigation }) {
   const [technologies, setTechnologies] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user_id => {
+      const socket = socketio('http://192.168.238.2:3333', {
+        query: { user_id }
+      });
+
+      socket.on('booking_response', booking => {
+        Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA' }`);
+      });
+    });
+  }, []);
 
   useEffect(() => {
     AsyncStorage.getItem('technologies').then(techs => {
